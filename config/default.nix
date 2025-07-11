4fe3480c701906d0107d4a2eca7e8c971cc71c29
Pieter Prinsloo <pietersp@gmail.com>
2025-07-11 19:13:17 +0200

    Some opts and shortcuts

--- config/default.nix
+++ config/default.nix
@@ -2,14 +2,33 @@
   # Import all your configuration modules here
   imports = [ 
     ./bufferline.nix 
-    ./catppuccin.nix
     ./metals.nix
     ./neo-tree.nix
     ./which-key.nix
   ];
 
+  colorschemes = {
+    catppuccin = {
+      enable = true;
+      settings = {
+        flavour = "mocha";
+      };
+    };
+  };
+
+  opts = {
+    number = true;
+    relativenumber = true;
+
+    termguicolors = true;
+  };
+
   globals = {
+    # Set <space> as the leader key
+    # See `:help mapleader`
     mapleader = " ";
     maplocalleader = " ";
+
+    have_nerd_font = true;
   };
 }
