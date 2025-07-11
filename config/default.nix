46bccaa4b05f8ba54d3e81406e11f85fda3c8116
Pieter Prinsloo <pietersp@gmail.com>
2025-07-11 19:52:35 +0200

    More settings and resturcture plugins directory

--- config/default.nix
+++ config/default.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: {
   # Import all your configuration modules here
   imports = [ 
-    ./bufferline.nix 
-    ./metals.nix
-    ./neo-tree.nix
-    ./which-key.nix
+    ./plugins/bufferline.nix 
+    ./plugins/metals.nix
+    ./plugins/neo-tree.nix
+    ./plugins/which-key.nix
   ];
 
   colorschemes = {
@@ -31,4 +31,59 @@
 
     have_nerd_font = true;
   };
+
+  plugins = {
+    # Detect tabstop and shiftwidth automatically
+    # https://nix-community.github.io/nixvim/plugins/sleuth/index.html
+    sleuth = {
+      enable = true;
+    };
+
+    # "gc" to comment visual regions/lines
+    # https://nix-community.github.io/nixvim/plugins/comment/index.html
+    comment = {
+      enable = true;
+    };
+
+    # Highlight todo, notes, etc in comments
+    # https://nix-community.github.io/nixvim/plugins/todo-comments/index.html
+    todo-comments = {
+      enable = true;
+      settings.signs = true;
+    };
+  };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<C-h>";
+      action = "<C-w><C-h>";
+      options = {
+        desc = "Move focus to the left window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-l>";
+      action = "<C-w><C-l>";
+      options = {
+        desc = "Move focus to the right window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-j>";
+      action = "<C-w><C-j>";
+      options = {
+        desc = "Move focus to the lower window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-k>";
+      action = "<C-w><C-k>";
+      options = {
+        desc = "Move focus to the upper window";
+      };
+    }
+  ];
 }
--- config/plugins/neo-tree.nix
+++ config/plugins/neo-tree.nix
@@ -21,7 +21,6 @@
         desc = "NeoTree Toggle";
       };
     }
-
   ];
 
 
