1882d8ae206fed82abd34b33a5e1cad4a6a48595
Pieter Prinsloo <pietersp@gmail.com>
2025-11-21 18:06:57 +0200

    Fix lspsaga changes

--- config/plugins/lspsaga.nix
+++ config/plugins/lspsaga.nix
@@ -1,56 +1,58 @@
 {
   plugins.lspsaga = {
     enable = true;
-    beacon.enable = true;
-    ui = {
-      border = "rounded"; # One of none, single, double, rounded, solid, shadow
-      codeAction = "💡"; # Can be any symbol you want 💡
-    };
-    hover = {
-      openCmd = "!floorp"; # Choose your browser
-      openLink = "gx";
-    };
-    diagnostic = {
-      borderFollow = true;
-      diagnosticOnlyCurrent = false;
-      showCodeAction = true;
-    };
-    symbolInWinbar.enable = true;
-    codeAction = {
-      extendGitSigns = false;
-      showServerName = true;
-      onlyInCursor = true;
-      numShortcut = true;
-      keys = {
-        exec = "<CR>";
-        quit = [ "<Esc>" "q" ];
+    settings = {
+      beacon.enable = true;
+      ui = {
+        border = "rounded"; # One of none, single, double, rounded, solid, shadow
+        codeAction = "💡"; # Can be any symbol you want 💡
       };
-    };
-    lightbulb.enable = false;
-    implement.enable = false;
-    rename = {
-      autoSave = false;
-      keys = {
-        exec = "<CR>";
-        quit = [ "<C-k>" "<Esc>" ];
-        select = "x";
+      hover = {
+        openCmd = "!floorp"; # Choose your browser
+        openLink = "gx";
       };
-    };
-    outline = {
-      autoClose = true;
-      autoPreview = false;
-      closeAfterJump = true;
-      layout = "normal"; # normal or float
-      winPosition = "right"; # left or right
-      keys = {
-        jump = "e";
-        quit = "q";
-        toggleOrJump = "o";
+      diagnostic = {
+        borderFollow = true;
+        diagnosticOnlyCurrent = false;
+        showCodeAction = true;
       };
-    };
-    scrollPreview = {
-      scrollDown = "<C-f>";
-      scrollUp = "<C-b>";
+      symbolInWinbar.enable = true;
+      codeAction = {
+        extendGitSigns = false;
+        showServerName = true;
+        onlyInCursor = true;
+        numShortcut = true;
+        keys = {
+          exec = "<CR>";
+          quit = [ "<Esc>" "q" ];
+        };
+      };
+      lightbulb.enable = false;
+      implement.enable = false;
+      rename = {
+        autoSave = false;
+        keys = {
+          exec = "<CR>";
+          quit = [ "<C-k>" "<Esc>" ];
+          select = "x";
+        };
+      };
+      outline = {
+        autoClose = true;
+        autoPreview = false;
+        closeAfterJump = true;
+        layout = "normal"; # normal or float
+        winPosition = "right"; # left or right
+        keys = {
+          jump = "e";
+          quit = "q";
+          toggleOrJump = "o";
+        };
+      };
+      scrollPreview = {
+        scrollDown = "<C-f>";
+        scrollUp = "<C-b>";
+      };
     };
   };
 
